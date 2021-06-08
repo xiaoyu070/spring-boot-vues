043c007ea57d77c50d94be4a8b93a45206bb4a9f
LAPTOP-H69LL4DV\86176 <2421074243@qq.com>
2021-06-08 21:34:27 +0800

    测试提交

--- src/test/java/com/trkj/projects/test.java
+++ src/test/java/com/trkj/projects/test.java
@@ -4,8 +4,6 @@ import com.trkj.projects.util.yan;
 
 public class test {
     public static void main(String[] args) {
-        yan yan=new yan();
-        String code=yan.yanzheng("13100232305",2);
-        System.out.println(code);
+
     }
 }
